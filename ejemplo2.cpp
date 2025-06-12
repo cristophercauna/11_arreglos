df76a14838a194676b104c9c8eb5e86e3734ea85
cristophercauna <ccaunam@unjbg.edu.pe>
2025-06-11 21:05:51 -0500

    Agregar ejercicio2.cpp: Se añade el mensaje que señala la cantidad de elementos que se ingresaran y el promedio de los elementos

--- ejemplo2.cpp
+++ ejemplo2.cpp
@@ -3,12 +3,14 @@ using namespace std;
 int main(){
     int n,suma,i,prom,elementos[20];
     cin>>n;
+    cout<<"Ingrese la cantidad de elementos: "<<endl;
     suma=0;
     for (i=0;i<n;i++){
         cin>>elementos[i];
         suma=suma+elementos[i];
     }
     prom=suma/n;
+    cout<<"El promedio de los elementos es: "<<prom<<endl;
     for(i=0;i<n;i++){
         if(elementos[i]>prom){
             cout<<elementos[i];
